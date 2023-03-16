83fb89ef04d510fe5d9c7fa5c244741cde294e69
Subroto Kumer Deb Nath <subroto.ece.ku@gmail.com>
2023-03-16 05:47:04 -0600

    Always Block Detection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
+
 ast = open('nerv.json')   #To open the json file
 data = json.load(ast)
-print(type(data))
+ast.close()
 data1 = data['members']
 
 def input_ports():     #Input ports identification function
@@ -13,8 +14,31 @@ def input_ports():     #Input ports identification function
                  print(key['name'])
 
 
-print("The following are the Input Ports:")
-input_ports()
+
+
+def always_block():      #Always, AlwaysFF and AlwaysComb block identification and counting function
+    n_ab_ff = 0
+    n_ab_comb = 0
+    n_ab = 0
+    for val in data1:
+        if "Instance" == val['kind']:
+            val1 = val['body']
+            for key in val1['members']:
+                if "ProceduralBlock" == key['kind'] and "AlwaysFF" == key['procedureKind']:
+                    n_ab_ff += 1
+                elif "ProceduralBlock" == key['kind'] and "AlwaysComb" == key['procedureKind']:
+                    n_ab_comb += 1
+                elif "ProceduralBlock" == key['kind'] and "Always" == key['procedureKind']:
+                    n_ab += 1
+    print("Total Number of AlwaysFF Blocks: ")
+    print(n_ab_ff)
+    print("Total Number of AlwaysComb Blocks: ")
+    print(n_ab_comb)
+    print("Total Number of Always Blocks: ")
+    print(n_ab)
+
+
+
 
 def output_ports():      #Output ports identification function
   for val in data1:
@@ -25,20 +49,24 @@ def output_ports():      #Output ports identification function
                  print(key['name'])
 
 
-print("The following are the Output Ports:")
-output_ports()
-
 def module_name(kind):      #This function for detecting the module name.
     for keyval in data['members']:
         if kind == keyval['kind']:
             return keyval['name']
 
-if module_name("Instance") != "":
 
+
+
+def main():
     print("The Module Name is:", module_name("Instance"))
-else:
-    print("No Module Name has been found!")
+    print("\n")
+    print("The following are the Input Ports:")
+    input_ports()
+    print("\n")
+    print("The following are the Output Ports:")
+    output_ports()
+    print("\n")
+    always_block()
 
 
-
-ast.close()
+main()
